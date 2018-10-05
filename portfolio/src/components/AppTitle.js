b960e17bd2bc582cfd92ef33055a56b0aff4fdf7
JeongSeokHo <creaticoding@gmail.com>
2018-10-05 10:10:05 +0900

    bk(portfolio): backup all of thing about create-react-app, sass, material-ui

--- portfolio/src/components/AppTitle.js
+++ portfolio/src/components/AppTitle.js
@@ -1,10 +1,35 @@
 import React from "react";
-import '../css/AppTitle.css';
+import "../css/AppTitle.css";
+import AppBar from "@material-ui/core/AppBar";
 
-const AppTitle = ({message}) => {
-	return (
-		<h1 className="App-title">{message}</h1>
-	);
-}
+// Instantiation
+//const topAppBarElement = document.querySelector(".mdc-top-app-bar");
+//const AppBar = new AppBar(topAppBarElement);
 
+const AppTitle = ({ message }) => {
+  return (
+    <header class="mdc-top-app-bar">
+      <AppBar class="mdc-top-app-bar__row">
+        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+          <a href="#" class="material-icons mdc-top-app-bar__navigation-icon">
+            menu
+          </a>
+          <a href="#" class="material-icons mdc-top-app-bar__navigation-icon">
+            sdsd
+          </a>
+          <span class="mdc-top-app-bar__title">{message}</span>
+        </section>
+      </AppBar>
+    </header>
+  );
+};
+/*
+
+        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+          <a href="#" class="material-icons mdc-top-app-bar__navigation-icon">
+            menu
+          </a>
+          <span class="mdc-top-app-bar__title">{message}</span>
+        </section>
+*/
 export default AppTitle;
