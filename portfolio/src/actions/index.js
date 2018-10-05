c426223df2aa42cd42ad0a4cfb44a59a0ccd7a18
creaticoding <creaticoding@gmail.com>
2018-10-05 13:30:41 +0000

    reset(portfolio): reset again

--- portfolio/src/actions/index.js
+++
@@ -1,8 +0,0 @@
-import * as types from "./ActionTypes";
-
-export function changeMessage(message){
-	return {
-		type: types.CHANGE_MESSAGE,
-		message
-	};
-}
--- portfolio/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux";
-import messageReducer from "./messageReducer";
-
-const reducers = combineReducers({
-	messageReducer
-});
-
-export default reducers;
