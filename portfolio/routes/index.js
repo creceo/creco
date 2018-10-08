ab7aa3941d72b25618482d1bbc6520f5a9bb60c9
JeongSeokHo <creaticoding@gmail.com>
2018-10-08 10:01:24 +0900

    feat(list): add date file of last commit.

--- portfolio/routes/index.js
+++ portfolio/routes/index.js
@@ -46,14 +46,57 @@ router.get("/api/list/post", (req, res, next) => {
   })
     .then(result => {
       for (let i in result) {
-        console.log(result[i]);
+        result[i].html_url = result[i].html_url.replace("/blob/", "/commits/");
       }
-      return result.map(e => {
-        return {
-          name: e.name,
-          date: "2018.10.08 02:00:01"
-        };
-      });
+      return result;
+    })
+    .then(result => {
+      let promises = [];
+      for (let i in result)
+        promises.push(
+          new Promise(resolve => {
+            // https://github.com/CreatiCoding/KOREAWIKI/commits/master/nginx%20%ED%8A%9C%ED%86%A0%EB%A6%AC%EC%96%BC.md
+            https.get(
+              {
+                host: "github.com",
+                headers: { "user-agent": "CreatiCoding" },
+                path: result[i].html_url.substr(18),
+                port: 443
+              },
+              response => {
+                console.log("Got response: " + response.statusCode);
+                let body = "";
+                response.on("data", d => {
+                  body += d;
+                });
+                response.on("end", function() {
+                  let data = body.substr(body.indexOf("Commits on"));
+                  result[i].date = data
+                    .substr(10, data.substr(0, 100).indexOf(", 20") + 4)
+                    .trim();
+                  resolve(result[i]);
+                });
+              }
+            );
+          })
+        );
+      return Promise.all(promises);
+    })
+    .then(result => {
+      return result
+        .map(e => {
+          return {
+            name: e.name,
+            date: new Date(e.date)
+          };
+        })
+        .sort(function(a, b) {
+          return b.date - a.date;
+        })
+        .map(e => {
+          e.date = e.date.toLocaleDateString();
+          return e;
+        });
     })
     .then(result => {
       res.json(result);
