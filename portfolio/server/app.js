68aaac58621f6181844f79fc98e978c680a02617
JeongSeokHo <creaticoding@gmail.com>
2018-10-18 19:11:21 +0900

    feat(static): add static file

--- portfolio/server/app.js
+++ portfolio/server/app.js
@@ -18,8 +18,10 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "build")));
+
+if (process.env.mode !== "development") {
+  app.use("/web", express.static(path.join(__dirname, "build")));
+}
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
